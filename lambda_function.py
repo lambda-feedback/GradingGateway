8bc2590bcde4d9373d1f72bc0bc64c001dcad9fe
RabidSheep55 <tharreaupierre@gmail.com>
2021-09-08 12:47:17 +0200

    fix bug in body parsing

--- lambda_function.py
+++ lambda_function.py
@@ -10,6 +10,27 @@ def handler(event, context):
     """
 
     # Request data is in the event body as it comes from an AWS API Gateway
+    if not event.get("body", None):
+        return {
+            "error": {
+                "statusCode": 400,
+                "description": "Body not found. (request event needs a 'body' field)",
+            }
+        }
+
+    # Parse body
+    if type(event["body"]) != dict:
+        try:
+            event["body"] = json.loads(event["body"])
+        # Catch Decode errors and return the problems back to the requester.
+        except json.JSONDecodeError as e:
+            return {
+                "error": {
+                    "type": "JSONDecodeError",
+                    "description": "Couldn't decode data from `body`",
+                }
+            }
+
     command = event["body"].get("command")
 
     if command == "grade":
@@ -22,7 +43,7 @@ def handler(event, context):
         return {
             "error": {
                 "statusCode": 400,
-                "description": "Command either not given or invalid. (request body needs a 'command' field)",
+                "description": f"Command [{command}] either not given or invalid. (request body needs a 'command' field)",
             }
         }
 
